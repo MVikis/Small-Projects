da11d4d2b2497c3b3bd1cfc50f034c128474f1f4
Martin Viksten <vikstenmartin@gmail.com>
2020-10-12 14:15:29 +0100

    added names of nations and flags

--- src/components/API/APIApp.js
+++ src/components/API/APIApp.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import Form from 'C:/Users/lisel/source/repos/MVikis/Date-Timer/date-timer/src/components/Universal/Form.js'
+import ReactCountryFlag from "react-country-flag"
+import {countries} from 'country-data';
 
 export default function APIApp(){
 
@@ -16,17 +18,22 @@ export default function APIApp(){
 
       }
   }
-  console.log(data)
-  console.log(data.name)
+
 
     return(
-        <div>
+        <div className="overlay">
+            <h2 >Name Nationalize</h2>
            <Form AddItem={Get}/>
            
            {data.name!== undefined?
-           <div>
-        <span>{data.name}</span>
-            {data.country.map(country =><span>{country.country_id} </span>)}
+           <div className="box">
+        <h3 className="logo">{data.name}</h3>
+           {data.country.map((country, index )=><div className="box-item" key={index}><ReactCountryFlag countryCode={country.country_id}  style={{
+                    width: '8em',
+                    height: '8em',
+                }} svg/>
+                <h4>{countries[country.country_id].name}</h4>
+                 <h4>{Math.ceil(country.probability * 100)}%</h4></div>)}
            </div>
            
            
